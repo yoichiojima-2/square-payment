e917db1781bfd0e619b0ba723b34f8e0ab93aaad
Yoichi Ojima <yoichiojima@gmail.com>
2024-05-26 17:31:22 +0900

    simplify css

--- src/components/RecordView.jsx
+++ src/components/RecordView.jsx
@@ -1,14 +1,16 @@
 import { groupByUser } from "./aggregate";
+import { unit } from "./config";
 
 export const RecordsTotalView = ({ records }) => {
+  console.log(unit);
   return (
     <div>
       <h2>record</h2>
       {records.map((record, index) => (
         <div key={index} className="HStack">
-          <p className="RecordName">{record.name}</p>
-          <p className="RecordTitle">{record.title}</p>
-          <p className="RecordAmount">{record.amount}</p>
+          <p>{record.name}</p>
+          <p>{record.title}</p>
+          <p>{unit} {record.amount}</p>
         </div>
       ))}
     </div>
@@ -22,15 +24,19 @@ export const RecordsUserView = ({ records }) => {
     <div>
       <h2>record by user</h2>
       {recordByUser.map((user) => (
-        <div key={user.name} className="UserRecords">
+        <div key={user.name} className="Card">
           <div className="HStack">
             <h3>{user.name}</h3>
-            <h3>{user.totalAmount}</h3>
+            <h3>
+              {unit} {user.totalAmount}
+            </h3>
           </div>
           {user.records.map((record, index) => (
             <div key={index} className="HStack">
               <p>{record.title}</p>
-              <p>{record.amount}</p>
+              <p>
+                {unit} {record.amount}
+              </p>
             </div>
           ))}
         </div>
--- src/components/SquareView.jsx
+++ src/components/SquareView.jsx
@@ -1,4 +1,5 @@
 import { getUsersWithTasks, getAmountPerUser } from "./aggregate";
+import { unit } from "./config";
 
 export const SquareView = ({ records }) => {
   const users = getUsersWithTasks(records);
@@ -9,17 +10,24 @@ export const SquareView = ({ records }) => {
       <h2>square result</h2>
       <h3>per person: {amountPerUser}</h3>
       {users.map((user, index) => (
-        <div key={index} className="UserSquare">
+        <div key={index} className="Card">
           <div className="HStack">
             <h3>{user.name}</h3>
-            <h3>{user.totalAmount}</h3>
+            <h3>
+              {unit} {user.totalAmount}
+            </h3>
           </div>
-          <h4>task</h4>
+          <h4>todos</h4>
           <div>
             {user.tasks.map((task, index) => (
-              <p key={index}>
-                {task.from} to {task.to}: {task.amount}
-              </p>
+              <div className="HStack">
+                <label>
+                  <input type="checkbox"/>
+                </label>
+                <p key={index}>
+                  {task.from} to {task.to}: {unit} {task.amount}
+                </p>
+              </div>
             ))}
           </div>
         </div>
